731785d12aac73d31612898cd0fc6e8ccc7406bd
Nitish Kumar <nitish441999@gmail.com>
2024-12-05 15:14:53 +0530

    some

--- api/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-const app = express()
-const port =9000;
-app.use("/",(req, resp) =>{
-    resp.json({message:"Hello I am Express"})
-})
-app.listen(9000, () =>{
-    console.log(`starting server on port ${port}`)
-})
