d43aa7117185b18d02f16f4f9ec0a0936e1a4491
Nitish Kumar <nitish441999@gmail.com>
2024-12-11 17:36:06 +0530

    somechange

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -12,11 +12,11 @@ const route = express.Router();
 // get all employees route using GET method
 route.get("/getAllEmplyees", getAllEmployees);
 // get sigale employee route useing GET method
-route.get("/getSingaleEmployee/:id", userAuthMiddleware, getEmployee);
+// route.get("/getSingaleEmployee/:id", userAuthMiddleware, getEmployee);
 // Update Employee Route || PUT
-route.put("/updateEmployee/:id", userAuthMiddleware, updateEmployeeController);
+// route.put("/updateEmployee/:id", userAuthMiddleware, updateEmployeeController);
 //DeleteEmployee route || delete
-route.delete("/deleteEmployee/:id", userAuthMiddleware, deleteEmployeeController);
+// route.delete("/deleteEmployee/:id", userAuthMiddleware, deleteEmployeeController);
 
 
 
