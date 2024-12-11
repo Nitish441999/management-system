fcbaf3b97bda7c4b148e1e646b51c58d4cacf5c0
Nitish Kumar <nitish441999@gmail.com>
2024-12-11 17:39:58 +0530

    somechange

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -8,11 +8,11 @@ const {
 const userAuthMiddleware = require("../Middlewares/userMiddlewares");
 // const userAuthMiddleware = require("../Middlewares/userMiddlewares");
 
-const route = express.Router();
+const router = express.Router();
 // get all employees route using GET method
-route.get("/getAllEmplyees", getAllEmployees);
+// router.get("/getAllEmplyees", getAllEmployees);
 // get sigale employee route useing GET method
-// route.get("/getSingaleEmployee/:id", userAuthMiddleware, getEmployee);
+router.get("/getSingaleEmployee/:id", userAuthMiddleware, getEmployee);
 // Update Employee Route || PUT
 // route.put("/updateEmployee/:id", userAuthMiddleware, updateEmployeeController);
 //DeleteEmployee route || delete
@@ -21,4 +21,4 @@ route.get("/getAllEmplyees", getAllEmployees);
 
 
 
-module.exports = route;
+module.exports = router;
