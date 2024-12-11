a645f662893909a3e1e89f2aa5e24f4462528122
Nitish Kumar <nitish441999@gmail.com>
2024-12-11 16:13:30 +0530

    somechange

--- index.js
+++ index.js
@@ -16,20 +16,18 @@ connectDb();
 app.use(cors());
 app.use(express.json()); // To parse incoming requests with JSON payloads
 
-// Routes
-// app.use("/api/auth", require("./Router/userRouter")); // Ensure userRouter.js exists and is correctly defined
 
-// Optional: Uncomment other routes as needed
-app.use("/api/auth", require("./Router/AddEmployeeRouter"));
-app.use("/api/employee", require("./Router/UserRouter"));
-app.use("/api/department", require("./Router/DepartmentRouter"));
-app.use("/api/leave", require("./Router/LeaveRouter"));
-
-
-// Example root 
+// Example root route
 app.get("/", (req, res) => {
   return res.status(200).send("<h1>Hello Server Start</h1>");
 });
+
+// Optional: Uncomment other routes as needed
+// app.use("/api/v1/auth", require("../Router/AddEmployeeRouter"));
+// app.use("/api/v1/employee", require("../Router/UserRouter"));
+// app.use("/api/v1/department", require("../Router/DepartmentRouter"));
+// app.use("/api/v1/leave", require("../Router/LeaveRouter"));
+// app.use("/api/v1/resetPassword", require("../Router/ResetPasswordRouter"));
 
 const PORT = process.env.PORT || 8000; // Get port from environment or default to 8000
 
